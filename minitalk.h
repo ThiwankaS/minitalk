c86c189fb66664cd8cc96d06fd35a229aed52a40
Thiwanka Somachandra <thivankas@outlook.com>
2024-12-25 03:07:07 +0200

    Initializing the custome header file foe the project

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 13:54:02 by tsomacha          #+#    #+#             */
+/*   Updated: 2024/12/23 15:01:46 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdio.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
