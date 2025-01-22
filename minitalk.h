689b32450476f049f8cd614918583274fc36f230
thivankas@outlook.com <tsomacha@c1r1p5.hive.fi>
2025-01-22 17:21:23 +0200

    Complete the implemention of version 2.0

--- minitalk.h
+++ minitalk.h
@@ -6,16 +6,12 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 13:54:02 by tsomacha          #+#    #+#             */
-/*   Updated: 2024/12/26 17:34:50 by tsomacha         ###   ########.fr       */
+/*   Updated: 2025/01/22 17:01:48 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
-
-# define WORD_SIZE 8
-# define MASK_DEC 0x01
-# define MASK_ENC 0x80
 
 # include <signal.h>
 # include <stdbool.h>
@@ -26,6 +22,9 @@
 # include "libft.h"
 
 bool	ft_isvalid(char *str);
-char	*ft_straddchar(volatile char *str, char ch);
+void	response_handler(int signum);
+void	ft_encode(pid_t server_pid, char c);
+void	ft_send_message(pid_t server_pid, char *str);
+void	handle_signal(int sig, siginfo_t *info, void *context);
 
 #endif
--- test.sh
+++ test.sh
@@ -18,12 +18,14 @@ sleep 1 # Wait a moment to ensure the server starts
 
 # Define an array of test strings
 TEST_STRINGS=(
-	"test string"
+	"Lorem ipsum odor amet, consectetuer adipiscing elit. Lacus vel consectetur magna feugiat congue ultricies. Nec dapibus dictum luctus litora ultrices. Quam elit erat dis maximus nostra euismod ante arcu. Ultricies potenti accumsan ac vivamus mollis potenti! Venenatis accumsan eget varius sapien nunc pellentesque. Vehicula lacus parturient velit felis quam vestibulum.
+Suspendisse amet mi eget conubia fringilla posuere ad hac. Posuere non nibh sagittis ac lectus semper fames. Lacus ullamcorper parturient, non curae eget molestie vel. Ornare conubia sapien duis nibh montes? Ad non aliquam suspendisse himenaeos primis tempus mus. Nisi suscipit cursus viverra porttitor venenatis porttitor vitae vestibulum. Nibh dolor ad scelerisque suspendisse
+potenti arcu taciti? Quam phasellus ornare leo ornare parturient tristique facilisi praesent dignissim. Fames etiam nostra sollicitudin mattis elementum nam."
 )
 
 # Send each string to the server using the client
 for STRING in "${TEST_STRINGS[@]}"; do
-  for ((i=1; i<=20; i++)); do
+  for ((i=1; i<=10; i++)); do
     echo "Sending: $STRING (Iteration $i)"
     ./client $SERVER_PID "$STRING"
     sleep 1 # Wait briefly to ensure the server processes each message
